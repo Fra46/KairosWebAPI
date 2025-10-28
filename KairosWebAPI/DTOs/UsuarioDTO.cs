c30d15168f256590b6c01f6c88d9ea1582274332
22MAT11 <matorresonate@unicesar.edu.co>
2025-10-28 01:23:44 -0500

    Validación del campo Documento, correción de declaración del campo Documento, cambio en las migraciones

--- KairosWebAPI/DTOs/UsuarioDTO.cs
+++ KairosWebAPI/DTOs/UsuarioDTO.cs
@@ -1,10 +1,20 @@
-﻿namespace KairosWebAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KairosWebAPI.DTOs
 {
     public class UsuarioDTO
     {
         public int Id { get; set; }
+
+        [Required, StringLength(100)]
         public string Nombre { get; set; }
+
+        [Required, StringLength(30)]
         public string Tipo { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{7,10}$", ErrorMessage = "El número de documento debe tener entre 7 y 10 dígitos numéricos.")]
+        [StringLength(10, MinimumLength = 7)]
         public string Documento { get; set; }
     }
 }
--- KairosWebAPI/Migrations/20251028032551_ChangeUsuarioDocumentoToString.Designer.cs
+++ KairosWebAPI/Migrations/20251028032551_ChangeUsuarioDocumentoToString.Designer.cs
@@ -4,6 +4,7 @@ using KairosWebAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KairosWebAPI.Migrations
 {
     [DbContext(typeof(ConnectionContext))]
-    partial class ConnectionContextModelSnapshot : ModelSnapshot
+    [Migration("20251028032551_ChangeUsuarioDocumentoToString")]
+    partial class ChangeUsuarioDocumentoToString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
